ab1faa58e605c057f18bf5aa433d9920fa90976b
Daniel <danieljnt8@gmail.com>
2024-01-08 21:40:36 +0100

    updating run

--- run_fl.sh
+++ run_fl.sh
@@ -3,12 +3,12 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 echo "Starting server"
-python server.py --freeze 24 --img 1536 --adam --batch 1 --epochs 1 --data ./data/VisDrone.yaml --weights yolov5l.pt --hy data/hyps/hyp.VisDrone.yaml --cfg models/yolov5l-tph-plus.yaml --name fl_exp &
+python server.py --freeze 24 --img 1536 --adam --batch 1 --epochs 1 --data ./data/VisDrone.yaml --weights yolov5l.pt --hy data/hyps/hyp.VisDrone.yaml --cfg models/yolov5l-xs-tph.yaml --name fl_exp_2 &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 for i in $(seq 1 2); do
     echo "Starting client $i"
-    python client.py --freeze 24 --img 1536 --adam --batch 1 --epochs 1 --data ./data/VisDrone.yaml --weights yolov5l.pt --hy data/hyps/hyp.VisDrone.yaml --cfg models/yolov5l-tph-plus.yaml --name fl_exp --clientid "$i" &
+    python client.py --freeze 24 --img 1536 --adam --batch 1 --epochs 1 --data ./data/VisDrone.yaml --weights yolov5l.pt --hy data/hyps/hyp.VisDrone.yaml --cfg models/yolov5l-xs-tph.yaml --name fl_exp_2 --clientid "$i" &
 done
 
 # Enable CTRL+C to stop all background processes
